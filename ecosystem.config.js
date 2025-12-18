0e0210414ff5cdc829e0915263da547cc4385466
Ubuntu <ubuntu@ip-172-31-40-23.ap-northeast-2.compute.internal>
2025-12-18 01:00:17 +0000

    pm2 ecosystem init

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'back-single-sign-on-system',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      log_file: './logs/combined.log',
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      merge_logs: true
+    }
+  ]
+};
+
